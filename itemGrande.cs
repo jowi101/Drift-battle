e68a5b5caeea2323a1fc7a73db706e167b12dff8
jowi101 <43269184+jowi101@users.noreply.github.com>
2018-09-25 09:39:37 +0200

    script de hacerse grande

---
+++ itemGrande.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class itemGrande : MonoBehaviour {
+    float x;
+    float y;
+    float z;
+    Vector3 newSize;
+   
+
+     void Update()
+    {
+        x += 0.1f;
+        y += 0.1f;
+        z += 0.1f;
+     
+
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Grande")
+        {
+            transform.localScale += new Vector3(x, y, z);
+            gameObject.tag = "Muere";
+        }
+    }
+    
+       
+}
---
+++ muerteItem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class muerteItem : MonoBehaviour {
+
+    public GameObject personaje;
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Muere")
+        {
+            Destroy(personaje, 0);
+        }
+    }
+}

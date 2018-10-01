892c103b37c411efd70ec1e206963c8061ee01e6
jowi101 <43269184+jowi101@users.noreply.github.com>
2018-10-01 09:36:19 +0200

    Menu,SplashScreen

---
+++ kjbo/Assets/itemMuerte.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class itemMuerte : MonoBehaviour
+{
+
+    public GameObject personaje;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Destroy(personaje, 0);
+        }
+    }
+}
+    
+       
---
+++ kjbo/Assets/respawn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class respawn : MonoBehaviour
+{
+
+    public GameObject[] obj;
+    public float tiempoMin = 1.5f;
+    public float tiempoMax = 3f;
+    // Use this for initialization
+    void Start()
+    {
+        Generar();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void Generar()
+    {
+        Instantiate(obj[Random.Range(0, obj.Length)], transform.position, Quaternion.identity);
+        Invoke("Generar", Random.Range(tiempoMin, tiempoMax));
+    }
+}

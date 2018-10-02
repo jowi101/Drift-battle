49b5f38631002b3b0fde5540a08b9f6597f41a2c
ivan.lada@hotmail.com <TELIX1436>
2018-10-02 18:13:15 +0200

    movimiento sexy

--- android estudios/Assets/Scenes/mov.cs
+++ android estudios/Assets/Scenes/mov.cs
@@ -4,30 +4,48 @@ using UnityEngine;
 
 public class mov : MonoBehaviour
 {
-    private int forward;
-    float x;
-    float y;
-    private Rigidbody r;
-    // Use this for initialization
-    void Start()
-    {
-        r = transform.GetComponent<Rigidbody>();
-        x = transform.eulerAngles.y;
-    }
+	private int forward;
+	float x;
+	float ejey;
+	private Rigidbody r;
+	public float speed;
+	public float speedRotation;
+	public float turnSpeed = 50f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        transform.Translate(Vector3.forward);
-        x += Input.GetAxis("Horizontal");
-        y = Input.GetAxis("Vertical");
 
-        transform.Translate(Vector3.forward);
-        Quaternion rotation = Quaternion.Euler(0, x, 0);
-        transform.rotation = rotation;
+	// Use this for initialization
+	void Start()
+	{
+		speed = 100.0f;
+		r = transform.GetComponent<Rigidbody>();
+		x = transform.eulerAngles.y;
+		speedRotation = 200.0f;
 
-        Vector3 forward = new Vector3(0.0f, 0.0f, y);
-        r.AddForce(forward * 15);
+	}
 
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		transform.Translate(Vector3.forward*speed*Time.deltaTime);
+
+
+		transform.Translate(Vector3.forward);
+		Quaternion rotation = Quaternion.Euler(0, x* speedRotation*Time.deltaTime, 0);
+		transform.rotation = rotation;
+
+		Vector3 forward = new Vector3(0.0f, 0.0f, ejey);
+		r.AddForce(forward* speed*Time.deltaTime);
+		Touch myTouch = Input.GetTouch(0);
+		Touch[] myTouches = Input.touches;
+		for (int i = 0; i < Input.touchCount; i++);
+
+		if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+		{
+			
+				transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
+
+
+				transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+		}
+	}
 }
